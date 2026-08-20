ceaa7de41268f9b461ecbf44a69b2cf194d0990a
Rishika Bhanushali <88501367+rb-rishika@users.noreply.github.com>
2026-08-19 18:37:05 -0700

    Add: count-paths - submission-2

---
+++ Data Structures & Algorithms/count-paths/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+
+        dp=[[-1]*n for _ in range(m)] #m*n
+
+        def dfs(i,j):
+            if i==(m-1) and j==(n-1):
+                return 1
+            if i>=m or j>=n: #out of bounds
+                return 0
+            #below is where we do memoization
+            if dp[i][j] !=-1:
+                return dp[i][j]
+            dp[i][j]= dfs(i+1,j) + dfs(i,j+1)
+            return dp[i][j]
+
+
+        return dfs(0,0)
+
+
+
+        
+    
