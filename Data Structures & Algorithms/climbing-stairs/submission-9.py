61f7d4feab0498972793d5fd57b531ae25e66413
Rishika Bhanushali <88501367+rb-rishika@users.noreply.github.com>
2026-08-28 19:36:16 -0700

    Add: climbing-stairs - submission-9

---
+++ Data Structures & Algorithms/climbing-stairs/submission-9.py
@@ -0,0 +1,37 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+
+        #actual dp saving result
+        
+        if n<2:
+            return n
+        dp=[0]*(n+1)
+        dp[1]=1
+        dp[2]=2
+
+        for i in range(3, n+1):
+            dp[i]=dp[i-1]+dp[i-2]
+        return dp[n]
+
+        # if n<=2:
+        #     return n
+        # else:
+        #     first=1
+        #     second =1
+        #     for i in range(n-1):
+        #         third=first+second
+        #         first=second
+        #         second=third
+        #     return second
+
+
+        # def dfs(stair):
+        #     if stair==n: #we found one valid way
+        #         return 1
+        #     if stair>n:
+        #         return 0
+        #     return dfs(stair+1)+dfs(stair+2)
+        
+
+        # return dfs(0)
+     
